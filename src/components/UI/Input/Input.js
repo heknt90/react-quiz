c4c3297350782b68ad64f2e71a81183738d3e0c7
Nikita Efremov <heknt90@mail.ru>
2023-01-30 19:28:47 +0300

    added Input Component

---
+++ src/components/UI/Input/Input.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import classes from './Input.module.scss'
+
+const Input = props => {
+    const inputType = props.type || 'text'
+    const htmlFor = `${inputType}-${Math.random()}`
+
+    return (
+        <div className={ classes.Input }>
+            <label 
+                htmlFor={ htmlFor }>
+                    { props.label }
+            </label>
+            <input 
+                type={ inputType } 
+                id={ htmlFor }
+                value={ props.value }
+                onChange={ props.changeHandler }  />
+        </div>
+    );
+};
+
+export default Input;
--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,29 +1,69 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Button from '../../components/UI/Button/Button';
+import Input from '../../components/UI/Input/Input';
 import classes from './Auth.module.scss'
 
-const Auth = props => {
-    const cls = [
-        classes.Auth,
-        'page'
-    ]
-    return (
-        <div className={cls.join(' ')}>
-            <h1>Авторизация</h1>
+const cls = [
+    classes.Auth,
+    'page'
+]
 
-            <form>
-                <input type="email" />
-                <input type="password" />
+export default class Auth extends Component {
 
-                <Button 
-                    type="primary"
+    state = {
+        value:  '',
+        formControls: {
+            email: '',
+            password: ''
+        }
+    }
 
-                    >
-                        Авторизация
-                </Button>
-            </form>
-        </div>
-    );
+    changeHandler = (event) => {
+        console.log('changed input value', event.target.type)
+        const formControls = this.state.formControls
+        formControls[event.target.type] = event.target.value
+        this.setState({ formControls })
+    }
+
+    loginHandler = () => {
+
+    }
+
+    registerHandler = () => {
+
+    }
+
+    render() {
+        return (
+            <div className={cls.join(' ')}>
+                <h1>Авторизация</h1>
+    
+                <form>
+                    <Input 
+                        type="email"
+                        label="Введите ваш email" 
+                        value={ this.state.formControls.email }
+                        changeHandler={ event => this.changeHandler(event) } />
+                    <Input 
+                        type="password"
+                        label="Введите пароль" 
+                        value={ this.state.formControls.password }
+                        changeHandler={ event => this.changeHandler(event) } />
+    
+                    <Button 
+                        type="success"
+    
+                        >
+                            Авторизация
+                    </Button>
+                    <Button 
+                        type="primary"
+    
+                        >
+                            Зарегистрироваться
+                    </Button>
+                </form>
+            </div>
+        );
+    }
 }
-
-export default Auth;
