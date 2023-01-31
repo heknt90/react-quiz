5cbb5fe5ea5cf763223bccffd4106a9bd0bfdaac
Nikita Efremov <heknt90@mail.ru>
2023-01-31 13:02:10 +0300

    added Input errorMessage

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -5,8 +5,25 @@ const Input = props => {
     const inputType = props.type || 'text'
     const htmlFor = `${inputType}-${Math.random()}`
 
+    const cls = [
+        classes.Input,
+    ]
+    if ( props.isTouched && props.isInvalid ) {
+        cls.push(classes.error)
+    }
+
+    const renderErrorMessage = () => {
+        return ( props.isTouched && props.isInvalid )
+            ? (
+                <span className={ classes.errorMessage }>
+                { props.errorMessage }
+                </span>
+            )
+            : null        
+    }
+
     return (
-        <div className={ classes.Input }>
+        <div className={ cls.join(' ') }>
             <label 
                 htmlFor={ htmlFor }>
                     { props.label }
@@ -16,6 +33,8 @@ const Input = props => {
                 id={ htmlFor }
                 value={ props.value }
                 onChange={ props.changeHandler }  />
+            { renderErrorMessage() }
+            
         </div>
     );
 };
