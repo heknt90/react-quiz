67cc90709b32c45f77dff31f3e0b23121cdecd1d
Nikita Efremov <heknt90@mail.ru>
2023-01-31 08:15:06 +0300

    added state and renderQuiz method to QuizList

--- src/containers/QuizList/QuizList.js
+++ src/containers/QuizList/QuizList.js
@@ -1,38 +1,39 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import classes from './QuizList.module.scss'
 
 const cls = [
     classes.QuizList,
     'page'
 ]
+export default class QuizList extends Component {
+    state = {
+        quizes: [ 1, 2, 3 ]
+    }
 
-const QuizList = props => {
+    renderQuiz = () => {
+        return this.state.quizes.map( (quiz, index) => {
+            return (
+                <li key={index}>
+                    <Link to={ "quiz/" + quiz }> Test { quiz } </Link>
+                </li>
+            )
+        })
+    }
 
-    return (
-        <div className={cls.join(' ')}>
-            <div>
-                <h1>Список викторин</h1>
+    render() {
+        console.log( this.renderQuiz() )
 
-                <ul>
-                    <li>
-                        <a href='#'>
-                            Test 1
-                        </a>
-                    </li>
-                    <li>
-                        <a href='#'>
-                            Test 2
-                        </a>
-                    </li>
-                    <li>
-                        <a href='#'>
-                            Test 3
-                        </a>
-                    </li>
-                </ul>
+        return (
+            <div className={cls.join(' ')}>
+                <div>
+                    <h1>Список викторин</h1>
+
+                    <ul>
+                        { this.renderQuiz() }
+                    </ul>
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
-
-export default QuizList;
